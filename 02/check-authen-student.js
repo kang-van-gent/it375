2afd36d01bc350323ce8b6c69e27430431974021
kang <kangvangent@gmail.com>
2020-08-16 13:04:35 +0700

    check-auth-student

--- 02/check-authen-student.js
+++ 02/check-authen-student.js
@@ -1,3 +1,36 @@
-const getAuthResult = async()=>{
+const  getAuthResult = () => {
+    const checkAuth = async (id, password) => {
+        return await new Promise((resolve, reject) => {
+            setTimeout(() => {
+                let authData = "User Authenticate";
+                resolve(authData);
+            }, 1000);
+        });
+    };
 
-}
+    const getStudentData = async (authData) => {
+        checkAuth()
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((err) => {
+                console.log(error.message);
+            });
+
+        return  await new Promise((resolve, reject) => {
+            setTimeout(() => {
+                let name = "Kang";
+                let permission = "member";
+                resolve({ name: name, permission: permission });
+            }, 2000);
+        });
+    };
+    getStudentData()
+        .then((result) => {
+            console.log(result);
+        })
+        .catch((err) => {
+            console.log(err.message);
+        });
+};
+getAuthResult();
